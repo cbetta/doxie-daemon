7a06ad692c6f5fb0ab4dec80f2385989b2104d2c
Cristiano Betta <cristiano@betta.io>
2016-05-30 13:07:03 +0100

    Update doxied.rb

--- doxied.rb
+++ doxied.rb
@@ -2,24 +2,30 @@ require 'doxie'
 require 'doxie/scanner'
 
 path = '/home/momo/Dropbox/Scans/'
+last_scan = 0
 
 loop do
   begin
     ips = Doxie::Scanner.ips
     ips.each do |ip|
       client = Doxie::Client.new(ip: ip)
-      client.scans.each_with_index do |scan, index|
+      scans = client.scans
+      scans.each do |scan|
+        this_scan = DateTime.parse(scan["modified"]).strftime("%s").to_i 
+        next if this_scan <= last_scan
         scan_name = scan["name"]
         time = Time.now.strftime('%Y-%m-%d_%H-%M-%S')
-        client.scan scan_name, "#{path}#{time}_#{index}.jpg"
-        puts "Saved #{scan_name} to #{path}#{time}_#{index}.jpg"
-        client.delete_scan scan_name
-        puts "Deleted #{scan_name}"
+        client.scan scan_name, "#{path}#{time}.jpg"
+        puts "Saved #{scan_name} to #{path}#{time}.jpg"
+        last_scan = this_scan
       end
+      scan_names = scans.map{|scan| scan["name"] } 
+      client.delete_scans scan_names
+      puts "Delete scans: #{scan_names}"
     end
   rescue Exception => e  
     puts e
   ensure
-    sleep(10)
+    sleep(20)
   end
 end
